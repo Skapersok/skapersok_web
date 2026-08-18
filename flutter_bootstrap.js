211af7c43dbf80080a0614359ea5524470e172fc
trymbf <trymbf@users.noreply.github.com>
2026-08-18 07:30:05 +0000

    deploy: Skapersok/skapersok_app@a4725097d7be453ff1e38a12df5b96f48078bcfb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2670962403" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2142406575" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
